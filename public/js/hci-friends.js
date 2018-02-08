8701714d3b11073712fb41caf6b6cb360b4823bd
Austin Rossow <arossow@ucsd.edu>
2018-02-08 10:55:28 -0800

    Import css js bootsrap, add interactivity.

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
+	$(".name").click(function(e) {
+		$(this).text(anagrammedName($(this).text()));
+	  e.preventDefault();
+	});
+	
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,8 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	data.friends.push({
+		"description" : req.query.description, "name" : req.query.name, "imageURL" : "http://lorempixel.com/400/400/people"
+	})
+	res.render('index', data)
  }
